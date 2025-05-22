30531f1cc6a96f387e70ea6891c890dface18e4a
Bernardo_Filho <berna10.fernandes@gmail.com>
2025-05-22 00:18:43 -0300

    feat: criação classe TaskDao.kt

--- app/src/main/java/com/example/personaltasks/data/TaskDao.kt
+++ app/src/main/java/com/example/personaltasks/data/TaskDao.kt
@@ -1,4 +1,18 @@
 package com.example.personaltasks.data
 
-class TaskDao {
+import androidx.room.*
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM task_table")
+    suspend fun getAllTasks(): List<Task>
+
+    @Insert
+    suspend fun insert(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Delete
+    suspend fun delete(task: Task)
 }
