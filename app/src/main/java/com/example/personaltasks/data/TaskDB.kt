5690ddf8883b16c8874235a052209f47dabf74ed
Bernardo_Filho <berna10.fernandes@gmail.com>
2025-05-22 00:37:04 -0300

    feat: criação classe RaskDB.kt

--- app/src/main/java/com/example/personaltasks/data/TaskDB.kt
+++ app/src/main/java/com/example/personaltasks/data/TaskDB.kt
@@ -1,4 +1,27 @@
 package com.example.personaltasks.data
 
-class TaskDB {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        private var INSTANCE: TaskDatabase? = null
+
+        fun getDatabase(context: Context): TaskDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "task_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
