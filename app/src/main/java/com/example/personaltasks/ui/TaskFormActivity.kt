ba8965a0e753216b82e5001a0b378e77df8366ef
Bernardo_Filho <berna10.fernandes@gmail.com>
2025-05-22 17:18:30 -0300

    feat: criação TaskFormActivity.kt

--- app/src/main/java/com/example/personaltasks/ui/TaskFormActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/TaskFormActivity.kt
@@ -1,4 +1,18 @@
 package com.example.personaltasks.ui
 
-class TaskFormActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.personaltasks.R
+
+// imports...
+
+class TaskFormActivity : AppCompatActivity() {
+    // campos: edtTitle, edtDescription, txtSelectedDate, etc.
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_form)
+
+        // lógica para salvar/editar tarefa e devolver via Intent
+    }
 }
