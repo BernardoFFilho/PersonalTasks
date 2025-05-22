1a91b4ad07fc459c58583c5bb24ece2ad97e15c4
Bernardo_Filho <berna10.fernandes@gmail.com>
2025-05-22 00:01:15 -0300

    feat: criação classe Task.kt

--- app/src/main/java/com/example/personaltasks/data/Task.kt
+++ app/src/main/java/com/example/personaltasks/data/Task.kt
@@ -1,4 +1,12 @@
 package com.example.personaltasks.data
 
-class Task {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task_table")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val description: String,
+    val deadline: String
+)
