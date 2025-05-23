d3d591fd4f053177ec7b5bf1fc07e056ef3502ea
Bernardo_Filho <berna10.fernandes@gmail.com>
2025-05-22 21:06:30 -0300

    fix: Tentativa sem sucesso de corrigir a configuração do KSP, APK nao roda devido a esse problema.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,9 @@
 import org.jetbrains.kotlin.gradle.internal.Kapt3GradleSubplugin.Companion.findKaptConfiguration
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -55,9 +54,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    add("ksp", libs.room.compiler)
+    implementation(libs.kotlin.coroutines.android)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
